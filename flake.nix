2579d7dfbdfdc6b50d30a7c41efcc336641e7575
João Capucho <jcapucho7@gmail.com>
2023-11-01 14:17:28 +0000

    Add flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cacert
+          binutils
+          xorg.xauth
+          xorg.xhost
+          openssh
+          curl
+          debootstrap
+          gnumake
+          wget
+        ];
+      };
+    });
+}
